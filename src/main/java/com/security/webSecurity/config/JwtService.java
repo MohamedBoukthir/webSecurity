55b8381eb97d1558ebf4bb2242bef8a974b6036b
MohamedBoukthir <medboukthir7@gmail.com>
2024-01-15 18:14:02 +0100

    Initial commit

---
+++ src/main/java/com/security/webSecurity/config/JwtService.java
@@ -0,0 +1,33 @@
+package com.security.webSecurity.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "JwTo3J4fuSz1CS2nK9GCQsXWTVal/bCQvZRu9oUIeRpQ2v8iuq7ldOy/PKcwr+kf\n";
+
+    public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyByte = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyByte);
+    }
+}
