4f3507f3b20d58385641c7f084aa05d78c7d9185
MohamedBoukthir <medboukthir7@gmail.com>
2024-01-16 20:50:08 +0100

    Create The AuthenticationController

---
+++ src/main/java/com/security/webSecurity/auth/AuthenticationService.java
@@ -0,0 +1,17 @@
+package com.security.webSecurity.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+
+    public AuthenticationResponse register(RegisterRequest request) {
+
+    }
+
+    public AuthenticationResponse authenticate(authenticateRequest request) {
+    }
+}
---
+++ src/main/java/com/security/webSecurity/auth/authenticateRequest.java
@@ -0,0 +1,8 @@
+package com.security.webSecurity.auth;
+
+public class authenticateRequest {
+
+    private String email;
+    private String password;
+
+}
