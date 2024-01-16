caf3bb7d051c6801a42db8f8e51d30a291ec6f92
MohamedBoukthir <medboukthir7@gmail.com>
2024-01-16 20:57:22 +0100

    Register method Implementation

--- src/main/java/com/security/webSecurity/auth/AuthenticationService.java
+++ src/main/java/com/security/webSecurity/auth/AuthenticationService.java
@@ -1,15 +1,34 @@
 package com.security.webSecurity.auth;
 
+import com.security.webSecurity.config.JwtService;
+import com.security.webSecurity.user.Role;
+import com.security.webSecurity.user.User;
+import com.security.webSecurity.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthenticationResponse register(RegisterRequest request) {
-
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthenticationResponse authenticate(authenticateRequest request) {
