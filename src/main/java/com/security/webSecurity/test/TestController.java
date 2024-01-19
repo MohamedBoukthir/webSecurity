3e6177b3902d845c6a36d2413878622a254a70e7
MohamedBoukthir <medboukthir7@gmail.com>
2024-01-19 18:09:34 +0100

    Create Role and Permission

--- src/main/java/com/security/webSecurity/test/TestController.java
+++ src/main/java/com/security/webSecurity/test/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/test/test-controller")
+@RequestMapping("/api/v1/test-controller")
 public class TestController {
 
     @GetMapping
